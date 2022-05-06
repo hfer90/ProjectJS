dfc844297aa5ff1db05c76a7a0bea428c45a5487
hfer90 <hfer.90@gmail.com>
2022-05-05 21:33:08 -0300

    clase 4 fin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import './App.css'; // ver
 import { NavBar } from "./components/Navbar/Navbar";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'; // ver
 
 function App() {
   return (
